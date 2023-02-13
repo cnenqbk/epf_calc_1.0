88a77a3a3e0e7f659106866732bffed68505dd6b
YH <77180317+cnenqbk@users.noreply.github.com>
2023-02-13 13:49:24 +0800

    Update EPF Calculator.cpp
    update the function in the code and some grammar error

--- EPF Calculator.cpp
+++ EPF Calculator.cpp
@@ -1,4 +1,4 @@
-//this is a simple employees provident funds calculator write by YH using compound interest formula
+//this is a simple employee provident funds calculator write by YH using compound interest formula
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -13,22 +13,22 @@ int main()
     cout << "  --------------------------------------" << endl;
     cout << "   EPF Calculator 1.0 ---> Credit to YH" << endl;
     cout << "  --------------------------------------" << endl;
-    cout << "  Notice : This program only can count the current saving of EPF in the future with static dividens" << endl << endl;
+    cout << "  Notice : This program only can count the current saving of EPF in the future with static dividend" << endl << endl;
     cout << "\n  Your current EPF saving : RM ";
     cin >> present;
-    cout << "\n  Dividends of EPF (%): ";
+    cout << "\n  Dividend of EPF (%): ";
     cin >> r3turn;
     cout << "\n  How long your EPF saving (at least a year) : ";
     cin >> time;
     /*epf calculator function*/
     percent = r3turn / 100;
-    epf = present * pow((1 + (percent / day)), (day * time));
-    interest = present * pow((1 + (percent / day)), (day * time)) - present;
+    epf = present * pow((1 + (percent / day)), (day * 1));
+    interest = present * pow((1 + (percent / day)), (day * 1)) - present;
     /*final output*/
     cout << "\n\n\n  Your EPF interest after " << time << " years is RM ";
-    cout <<fixed<<setprecision(2) << interest << " with "<< r3turn <<" % dividens"<<endl;
+    cout <<fixed<<setprecision(2) << interest << " with "<< r3turn <<" % dividend"<<endl;
     cout << "\n  Your final EPF saving after "<< time <<" years is RM ";
-    cout <<fixed<<setprecision(2) << epf << " with " << r3turn << " % dividens"<< endl;
+    cout <<fixed<<setprecision(2) << epf << " with " << r3turn << " % dividend"<< endl;
 
     return 0; /*program will execute succesfully unless have a logical or syntax error, return 0 is not a must*/
 }
